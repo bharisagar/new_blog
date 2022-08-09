c8c132e0e17586e41adaca98c840a103b9fec158
punithraj-work <punithraj@dadutechnologies.comsssgit config --global user.email punithraj@dadutechnologies.com>
2022-08-09 13:24:29 +0530

    generated devise:views and added username in dropdown, working till 59:57

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers:{
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :posts
-  get 'pages/home'
-  get 'pages/about'
+
+  # get 'pages/home'
+  # get 'pages/about'
 
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
