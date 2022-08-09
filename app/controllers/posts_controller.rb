699bda672489721ccaaf991ba69044bca330c0e7
sagarbhari06@gmail.com <sagarbhari06@gmail.com>
2022-08-09 18:03:42 +0530

    code with git functionalities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,19 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
-
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[show index]
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user, :rich_text_body).order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
   def show
     @post.update(views: @post.views + 1)
+    @comments = @post.comments.includes(:user, :rich_text_body).order(created_at: :desc)
+
+    ahoy.track 'Viewed Post', post_id: @post.id
+
+    mark_notifications_as_read
   end
 
   # GET /posts/new
@@ -17,16 +22,16 @@ class PostsController < ApplicationController
   end
 
   # GET /posts/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +44,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,19 +58,32 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :body)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+
+    # If an old id or a numeric id was used to find the record, then
+    # the request slug will not match the current slug, and we should do
+    # a 301 redirect to the new path
+    redirect_to @post, status: :moved_permanently if params[:id] != @post.slug
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
+  end
+
+  def mark_notifications_as_read
+    if current_user
+      notifications_to_mark_as_read = @post.notifications_as_post.where(recipient: current_user)
+      notifications_to_mark_as_read.update_all(read_at: Time.zone.now)
     end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,33 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: {minimum: 5, maximum: 50}
-    validates :body, presence: true, length: {minimum: 10, maximum: 100}
-end
+    extend FriendlyId
+    validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+    validates :body, presence: true
+    belongs_to :category
+    has_rich_text :body
+    belongs_to :category
+  
+    belongs_to :user
+    has_many :comments, dependent: :destroy
+  
+    has_rich_text :body
+    has_one :content, class_name: 'ActionText::RichText', as: :record, dependent: :destroy
+  
+    has_noticed_notifications model_name: 'Notification'
+    has_many :notifications, through: :user
+  
+    friendly_id :title, use: %i[slugged history finders]
+  
+    def should_generate_new_friendly_id?
+      title_changed? || slug.blank?
+    end
+  
+    def views_by_day
+      daily_events = Ahoy::Event.where("cast(properties ->> 'post_id' as bigint) = ?", id)
+      daily_events.group_by_day(:time, range: 1.month.ago..Time.now).count
+    end
+  
+    def self.total_views_by_day
+      daily_events = Ahoy::Event.where(name: 'Viewed Post')
+      daily_events.group_by_day(:time, range: 1.month.ago..Time.now).count
+    end
+  end
